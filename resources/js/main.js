31f45662e7339be397dbe8c68a56a713ff077f05
Katrin <webentwicklerinKatrin@gmail.com>
2017-09-21 17:14:50 +0200

    carousel

---
+++ resources/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $('.carousel').slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    slidesToScroll: 1,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    draggable: false
+  });
+});
